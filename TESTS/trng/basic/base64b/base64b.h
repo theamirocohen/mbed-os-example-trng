3890c3a8dd200c8e4fb9ee76c8494ff96b3e1783
Amir Cohen <the.amiro.cohen@gmail.com>
2018-05-30 17:12:45 +0300

    Fix style issues

--- TESTS/trng/basic/base64b/base64b.h
+++ TESTS/trng/basic/base64b/base64b.h
@@ -23,10 +23,10 @@ typedef enum {
     BASE64_INVALID_PARAMETER = 1,
     BASE64_BUFFER_TOO_SMALL = 2,
     BASE64_ERROR = 3,
-}base64_result_e;
+} base64_result_e;
 
-base64_result_e esfs_EncodeBase64(const void* buffer, uint32_t bufferSize, char* string, uint32_t stringSize);
-base64_result_e esfs_DecodeNBase64(const char* string, uint32_t stringMaxSize, void* buffer, uint32_t bufferSize, 
-                                 uint32_t* lengthWritten, uint32_t* charsProcessed);
+base64_result_e esfs_EncodeBase64(const void *buffer, uint32_t bufferSize, char *string, uint32_t stringSize);
+base64_result_e esfs_DecodeNBase64(const char *string, uint32_t stringMaxSize, void *buffer, uint32_t bufferSize,
+                                   uint32_t *lengthWritten, uint32_t *charsProcessed);
 
 
